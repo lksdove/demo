5c1f67837a7d63dbd33b396ec97f657309567bcc
lks <741153798@qq.com>
2021-07-24 08:06:05 +0800

    mac本地修改提交0724

--- src/main/java/com/zzu/demo/redis/RedisTest.java
+++ src/main/java/com/zzu/demo/redis/RedisTest.java
@@ -1,47 +1,47 @@
-package com.zzu.demo.redis;
-
-import com.alibaba.fastjson.JSON;
-import com.zzu.demo.entity.User;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import redis.clients.jedis.Jedis;
-
-/**
- * @author lks
- * @date 2021/6/2 14:24
- */
-@Slf4j
-@RestController
-@RequestMapping("redis")
-public class RedisTest {
-
-    @PostMapping("setRedis")
-    public String setRedis() {
-        Jedis jedis = new Jedis();
-        jedis.auth("123456");
-        log.info("连接redis成功");
-        User user = new User();
-        user.setId(100);
-        user.setName("qwe");
-        user.setAge(200);
-        String userStr = JSON.toJSONString(user);
-        jedis.set("name", userStr);
-        log.info("set成功");
-        return "success";
-    }
-
-    @GetMapping("getRedis")
-    public String getRedis() {
-        Jedis jedis = new Jedis();
-        jedis.auth("123456");
-        log.info("连接redis成功");
-        String name = jedis.get("name");
-        User parseObject = JSON.parseObject(name, User.class);
-        System.err.println(parseObject.toString());
-        log.info("get成功");
-        return name;
-    }
-}
+//package com.zzu.demo.redis;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.zzu.demo.entity.User;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import redis.clients.jedis.Jedis;
+//
+///**
+// * @author lks
+// * @date 2021/6/2 14:24
+// */
+//@Slf4j
+//@RestController
+//@RequestMapping("redis")
+//public class RedisTest {
+//
+//    @PostMapping("setRedis")
+//    public String setRedis() {
+//        Jedis jedis = new Jedis();
+//        jedis.auth("123456");
+//        log.info("连接redis成功");
+//        User user = new User();
+//        user.setId(100);
+//        user.setName("qwe");
+//        user.setAge(200);
+//        String userStr = JSON.toJSONString(user);
+//        jedis.set("name", userStr);
+//        log.info("set成功");
+//        return "success";
+//    }
+//
+//    @GetMapping("getRedis")
+//    public String getRedis() {
+//        Jedis jedis = new Jedis();
+//        jedis.auth("123456");
+//        log.info("连接redis成功");
+//        String name = jedis.get("name");
+//        User parseObject = JSON.parseObject(name, User.class);
+//        System.err.println(parseObject.toString());
+//        log.info("get成功");
+//        return name;
+//    }
+//}
